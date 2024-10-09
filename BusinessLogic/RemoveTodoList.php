4ff4b92267dcee11c16a25c6971013e26090a409
fsAlfian <alfian.horis@mcm.id>
2024-10-09 11:48:59 +0700

    add logic for remove todolist

--- BusinessLogic/RemoveTodoList.php
+++ BusinessLogic/RemoveTodoList.php
@@ -1,5 +1,15 @@
 <?php
 
-function removeTodoList(){
-    
+function removeTodoList(int $number): bool {
+    global $todoList;
+
+    if (sizeof($todoList) < $number) {
+        return false;
+    }
+
+    for ($i = $number; $i < sizeof($todoList); $i++) {
+        $todoList[$i] = $todoList[$i + 1];
+    }
+    unset($todoList[sizeof($todoList)]);
+    return true;
 }
