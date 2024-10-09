2061bbe10cd171d9c428e976da4d43dbe9686aa8
fsAlfian <alfian.horis@mcm.id>
2024-10-09 11:37:15 +0700

    add test for show todo list

---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "../Model/Todolist.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+
+$todoList[1] = "Learn PHP OOP";
+$todoList[2] = "Learn PHP DB";
+
+showTodoList();
