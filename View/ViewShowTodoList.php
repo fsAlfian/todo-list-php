c0f9915d2846956547dcdb077a4812837d82f5ea
fsAlfian <alfian.horis@mcm.id>
2024-10-09 13:23:24 +0700

    add view show todo list

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,4 +1,28 @@
 <?php
+
+require_once '../BusinessLogic/ShowTodoList.php';
+require_once '../Helper/Input.php';
+require_once '../BusinessLogic/AddTodoList.php';
+require_once '../BusinessLogic/RemoveTodoList.php';
+
 function viewShowTodoList() {
+    while(true) {
+        showTodoList();
+        echo '"Menu"' . PHP_EOL;
+        echo "1. Add Todo" . PHP_EOL;
+        echo "2. Remove Todo" . PHP_EOL;
+        echo "3. Exit" . PHP_EOL;
     
+        $choice = input("Input your choice");
+        if ($choice == "1") {
+            viewAddTodoList();
+        } else if ($choice == "2") {
+            viewRemoveTodoList();
+        } else if ($choice == "3") {
+            // exit
+            break;
+        } else {
+            echo "Wrong choice" . PHP_EOL;
+        }
+    }
 }
