daeb71b1cfc54cf510fe643d11d55b6e29cc909c
fsAlfian <alfian.horis@mcm.id>
2024-10-09 11:34:04 +0700

    add show todolist

--- App.php
+++ App.php
@@ -3,5 +3,8 @@ require_once "model/Todolist.php";
 require_once "BusinessLogic/AddTodoList.php";
 require_once "BusinessLogic/ShowTodoList.php";
 require_once "BusinessLogic/RemoveTodoList.php";
+require_once "View/ViewShowTodoList.php";
+require_once "View/ViewAddTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
 
 echo "Todo list app" . PHP_EOL;
--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -3,5 +3,10 @@
 //show todo list
 
 function showTodoList(){
-    
+    global $todoList;
+
+    echo "TODOLIST" . PHP_EOL;
+    foreach ($todoList as $number => $value) {
+        echo "$number. $value" . PHP_EOL;
+    }
 }
