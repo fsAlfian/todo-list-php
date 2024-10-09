5fae18902e42dd1624af7247662060340b6f02d1
fsAlfian <alfian.horis@mcm.id>
2024-10-09 13:47:19 +0700

    add view remove todo list

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,5 +1,16 @@
 <?php
 
+require_once '../model/Todolist.php';
+require_once '../Helper/Input.php';
+require_once '../BusinessLogic/RemoveTodoList.php';
+
 function viewRemoveTodoList(){
-    
+    echo "REMOVE TODO LIST" . PHP_EOL;
+    $number = input("Input todo number");
+    $success = removeTodoList($number);
+    if ($success) {
+        echo "Success remove todo list" . PHP_EOL;
+    } else {
+        echo "Failed remove todo list" . PHP_EOL;
+    }
 }
